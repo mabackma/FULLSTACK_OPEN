127bc76d88a6e9cbff47a9aba9f569338393a06f
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-08-09 05:21:28 +0300

    part2.1-2.5

---
+++ part2/course_information/src/Course.js
@@ -0,0 +1,29 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}  
+  </>
+
+const Total = ({parts}) =>
+  <>
+    <b>
+      total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
+    </b>
+  </>
+
+const Course = ({course}) => 
+  <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </>
+
+export default Course
